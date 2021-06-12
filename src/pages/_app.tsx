965f747df5456ae219fe35efb7a8729d862c8ea1
JoaoFerreiraa <joao.ferreira236@hotmail.com>
2021-06-12 17:49:31 -0300

    footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import theme from "ui/themes/theme";
 import Head from "next/head";
 
 import Header from "ui/components/surfaces/Header/Header";
+import Footer from "ui/components/surfaces/Footer/Footer";
+import { AppContainer } from "@styles/pages/_app.style";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -21,8 +23,11 @@ function MyApp({ Component, pageProps }) {
         <link href="/fonts/tw-icons/css/treinaweb-icons.css" rel="stylesheet" />
       </Head>
       <ThemeProvider theme={theme}>
-        <Header />
-        <Component {...pageProps} />
+        <AppContainer>
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </AppContainer>
       </ThemeProvider>
     </>
   );
